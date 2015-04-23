3d49d6c1dc518641738ed68f55db22c67035ba6f
alexfakhri <alexfakhri@me.com>
2015-04-23 12:55:36 +0100

    Displaying Stories

--- spec/features/articles_feature_spec.rb
+++ spec/features/articles_feature_spec.rb
@@ -12,4 +12,21 @@ feature 'article' do
 
   end
 
+  context 'articles have been added' do
+
+    before do
+      Article.create(title: 'One Awesome Story')
+    end
+
+    scenario 'Displaying an article' do
+      visit '/articles'
+      expect(page).to have_content 'One Awesome Story'
+      expect(page).not_to have_content 'No articles yet'
+    end
+
+  end
+
+
+
+
 end
