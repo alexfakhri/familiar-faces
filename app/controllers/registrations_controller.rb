2107b1179c1fa7da785254cc3fcd52108cb033c2
NicolePell <pellicena@gmail.com>
2015-04-24 14:27:47 +0100

    user can add a profile pic

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -7,6 +7,6 @@ class RegistrationsController < Devise::RegistrationsController
   end
 
   def account_update_params
-    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :bio)
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password, :bio, :avatar)
   end
 end
