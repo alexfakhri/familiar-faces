3443389452b5f1b15acfc39c4556083c7d9aa3f4
alexfakhri <alexfakhri@me.com>
2015-06-06 19:05:13 +0530

    Added Disqus commenting system to article views, untested, but working

--- Gemfile
+++ Gemfile
@@ -35,6 +35,8 @@ gem 'cancancan', '~> 1.10'
 
 gem 'best_in_place'
 
+gem 'disqus_rails'
+
 # Use ActiveModel has_secure_password
 # gem 'bcrypt', '~> 3.1.7'
 
--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,6 +14,7 @@
 //= require jquery_ujs
 //= require jquery.purr
 //= require best_in_place
+//= require disqus_rails
 //= require turbolinks
 //= require_tree .
 
