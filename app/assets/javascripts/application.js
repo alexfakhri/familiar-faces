ea04323a84aeb4b97e2e0319d952a95d0d4e7e3d
alexfakhri <alexfakhri@me.com>
2015-06-13 12:08:35 +0530

    Changing menu bar to match photoshop layouts

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -18,23 +18,23 @@
 //= require turbolinks
 //= require_tree .
 
-$(function() {
-    var pull        = $('#pull');
-        menu        = $('nav ul');
-        menuHeight  = menu.height();
-
-    $(pull).on('click', function(e) {
-        e.preventDefault();
-        menu.slideToggle();
-    });
-});
-
-$(window).resize(function(){
-    var w = $(window).width();
-    if(w > 320 && menu.is(':hidden')) {
-        menu.removeAttr('style');
-    }
-});
+// $(function() {
+//     var pull        = $('#pull');
+//         menu        = $('nav ul');
+//         menuHeight  = menu.height();
+//
+//     $(pull).on('click', function(e) {
+//         e.preventDefault();
+//         menu.slideToggle();
+//     });
+// });
+//
+// $(window).resize(function(){
+//     var w = $(window).width();
+//     if(w > 320 && menu.is(':hidden')) {
+//         menu.removeAttr('style');
+//     }
+// });
 
 $(document).ready(function() {
   /* Activating Best In Place */
